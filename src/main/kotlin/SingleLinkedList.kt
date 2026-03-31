29c9bfe5ddf8bd17701e25f18283f413332d9802
Elizaveta M <mefmag01@gmail.com>
2026-03-31 18:49:01 +0000

    Исправлен MR и удалены встроенные структуры. Реализован SingleLinkedList без использования innerList

--- src/main/kotlin/SingleLinkedList.kt
+++ src/main/kotlin/SingleLinkedList.kt
@@ -2,47 +2,102 @@ package org.example
 
 class SingleLinkedList : CustomList {
 
-    private val inner = mutableListOf<Int>()
+    private data class Node(var value: Int, var next: Node? = null)
+
+    private var head: Node? = null
+    private var _size: Int = 0
 
     override val size: Int
-        get() = inner.size
+        get() = _size
 
     override fun add(element: Int) {
-        inner.add(element)
+        if (head == null) {
+            head = Node(element)
+        } else {
+            var current = head
+            while (current!!.next != null) {
+                current = current.next
+            }
+            current.next = Node(element)
+        }
+        _size++
     }
 
     override operator fun set(index: Int, value: Int) {
-        inner[index] = value
+        if (index < 0 || index >= _size) {
+            throw IndexOutOfBoundsException("Index: $index, Size: $_size")
+        }
+        var current = head
+        for (i in 0 until index) {
+            current = current!!.next
+        }
+        current!!.value = value
     }
 
     override fun addFirst(element: Int) {
-        inner.add(0, element)
+        head = Node(element, head)
+        _size++
     }
 
     override operator fun get(index: Int): Int {
-        return inner[index]
+        if (index < 0 || index >= _size) {
+            throw IndexOutOfBoundsException("Index: $index, Size: $_size")
+        }
+        var current = head
+        for (i in 0 until index) {
+            current = current!!.next
+        }
+        return current!!.value
     }
 
 
     override fun indexOf(element: Int): Int {
-        return inner.indexOf(element)
+        var current = head
+        var index = 0
+        while (current != null) {
+            if (current.value == element) {
+                return index
+            }
+            current = current.next
+            index++
+        }
+        return -1
     }
 
     override fun remove(element: Int): Boolean {
-        return inner.remove(element)
+        if (head == null) return false
+
+        if (head!!.value == element) {
+            head = head!!.next
+            _size--
+            return true
+        }
+
+        var current = head
+        while (current!!.next != null) {
+            if (current.next!!.value == element) {
+                current.next = current.next!!.next
+                _size--
+                return true
+            }
+            current = current.next
+        }
+        return false
     }
 
     override fun iterator(): Iterator<Int> {
         return object : Iterator<Int> {
-            private var currentIndex = 0
+            private var current = head
 
             override fun hasNext(): Boolean {
-                return currentIndex < inner.size
+                return current != null
             }
 
             override fun next(): Int {
                 if (!hasNext()) throw NoSuchElementException()
-                return inner[currentIndex++]
+                val value = current!!.value
+                current = current!!.next
+                return value
             }
         }
     }
